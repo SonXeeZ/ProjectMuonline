ceb392c29caf72907865d78a6cb2e55d6214aa48
ChronoMu <chronomuonline@gmail.com>
2023-10-26 18:36:47 +0200

    Updated SQL - need fix

--- Evi.Network.IGC.Server Suite.x6.0.0.1/Database/1. Scripts/MuOnline/MuOnline.sql
+++ Evi.Network.IGC.Server Suite.x6.0.0.1/Database/1. Scripts/MuOnline/MuOnline.sql
@@ -8913,13 +8913,15 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE FUNCTION [dbo].[fn_md5] (@data VARCHAR(20), @data2 VARCHAR(10))
-RETURNS BINARY(16) AS
+CREATE FUNCTION [dbo].[fn_md5] (@pass VARCHAR(10), @user VARCHAR(10))
+RETURNS VARBINARY(16) 
+AS
 BEGIN
-DECLARE @hash BINARY(16)
-EXEC master.dbo.XP_MD5_EncodeKeyVal @data, @data2, @hash OUT
-RETURN @hash
+    DECLARE @result VARBINARY(16)
+    EXEC master.dbo.XP_MD5_EncodeKeyVal @pass, @user, @result OUTPUT
+    RETURN @result
 END
+
 GO
 /****** Object:  UserDefinedFunction [dbo].[IGC_FriendChat_GetMessageLogs]    Script Date: 21/03/2016 13:53:00 ******/
 SET ANSI_NULLS ON
